bdc1e100bc89914ebcc6d5a27ac200d0b2fcd0d6
populary <410445919@qq.com>
2016-07-25 13:41:27 +0800

    Initial Commit

---
+++ iOSSomeView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  iOSSomeView
+//
+//  Created by yang on 16/7/25.
+//  Copyright © 2016年 poplary. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
